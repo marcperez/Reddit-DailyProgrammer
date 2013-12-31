18ef928e685b7c8ff66323d11868996e2ade61ec
Marc Pérez <marcperez2000@gmail.com>
2013-12-31 19:50:56 +0100

    easy_29 challenge method refactor to one line

--- easy_29/easy_29.rb
+++ easy_29/easy_29.rb
@@ -1,13 +1,9 @@
-#Your task is to write a function to determine whether 
+# Your task is to write a function to determine whether 
 # a given string is palindromic or not.
 
 
 def palindrome?(word)
-  if word == word.reverse
-    return true
-  end
-
-  return false
+  word == word.reverse
 end
 
 palindrome?("12321") # true
